30e1013ee99999ec474b3de06e1d22270625ea01
jameskinyua590 <jameskinyua590@gmail.com>
2025-02-11 09:25:00 +0000

    implement searchBy middleware

--- server/middleware/searchBy.js
+++ server/middleware/searchBy.js
@@ -1,12 +1,21 @@
 const WantedProfileModel = require('../repositories/models/WantedProfile');
+const paginator = require('../utils/paginator');
 
 async function searchBy(req, res, next) {
+    const { page = 1, limit = 10 } = req.query;
     const { search = '' } = req.params;
     if (!search) return next();
 
-    const results = await WantedProfileModel.find({ $or: [{ name: search }, { hair: search }] }).exec();
-    
+    const query = await WantedProfileModel.find({ $or: [{ name: search }, { hair: search }] });
+    const count = await WantedProfileModel.countDocuments({ $or: [{ name: search }, { hair: search }] }).exec();
 
+    const wantedProfiles = await paginator(query, page, limit);
+
+    return res.status(200).json({
+        wantedProfiles,
+        totalPages: Math.ceil(count / limit),
+        currentPage: page
+    });
 }
 
 module.exports = {
--- server/routes/getMostWanted.js
+++ server/routes/getMostWanted.js
@@ -2,16 +2,17 @@ const { Router } = require('express');
 const { WantedProfileModel } = require('../repositories/models/WantedProfile');
 const { ServiceUnavailableException } = require('../errors/503');
 const { paginate } = require('../utils/paginator');
+const { searchBy } = require('../middleware/searchBy');
 
 const mostWantedRoutes = Router();
 
-mostWantedRoutes.get('/', async (req, res) => {
+mostWantedRoutes.get('/', searchBy, async (req, res) => {
     const { page = 1, limit = 10 } = req.query;
     try {
         const query = await WantedProfileModel.find({});
         const wantedProfiles = await paginate(query, page, limit);
 
-        const count = await WantedProfileModel.countDocuments({});
+        const count = await WantedProfileModel.countDocuments({}).exec();
 
         res.status(200).json({
             wantedProfiles,
