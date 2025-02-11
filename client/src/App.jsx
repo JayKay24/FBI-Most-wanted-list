69a9dbc13574dde4e614ef308ef36e3af9285a8e
jameskinyua590@gmail.com <jameskinyua590@gmail.com>
2025-02-11 20:12:49 +0300

    eslint disable for importing react
    Signed-off-by: jameskinyua590@gmail.com <jameskinyua590@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import ListMostWantedProfiles from './pages/ListMostWantedProfiles';
--- client/src/pages/DisplayMostWantedProfile.jsx
+++ client/src/pages/DisplayMostWantedProfile.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import useFetchMostWantedProfiles from '../hooks/useFetchMostWantedProfiles';
